0be990de592c8ef16b7b762c6ab1798ac1c3b71c
OrsanZ <OrsanZ@users.noreply.github.com>
2020-07-28 18:04:51 +0200

    seperated constants from enums

--- headers/enums.h
+++ headers/enums.h
@@ -1,13 +1,28 @@
-char DARK_CORE_SPEED_FAST           = 0x03;
-char DARK_CORE_SPEED_NORMAL         = 0x02;
-char DARK_CORE_SPEED_SLOW           = 0x01;
+/* Corresponds to internal values for the Dark Core */
+enum DARK_CORE_SPEED
+{
+  /* Slow color cycling speed */
+  DARK_CORE_SPEED_SLOW = 0x01,
 
-char DARK_CORE_MODE_SHIFT           = 0x00;
-char DARK_CORE_MODE_PULSE           = 0x01;
-char DARK_CORE_MODE_RAINBOW         = 0x03;
-char DARK_CORE_MODE_STATIC          = 0x07;
-char DARK_CORE_MODE_DPI             = -0x02;
+  /* Normal color cycling speed */
+  DARK_CORE_SPEED_NORMAL = 0x02,
 
-char DARK_CORE_DPI_PROFILE_ON       = 0x0F;
-char DARK_CORE_DPI_PROFILE_OFF      = 0x0B;
-char DARK_CORE_DPI_PROFILE_OVERRIDE = 0x00;
+  /* Fast color cycling speed */
+  DARK_CORE_SPEED_FAST = 0x03
+};
+
+/* Lighting modes correspond to internal values for the Dark Core */
+enum DARK_CORE_LIGHTING_MODE
+{
+  /* Two colors shifting from one to another */
+  DARK_CORE_LIGHTING_MODE_SHIFT = 0x00,
+
+  /* Two alternating colors pulsing */
+  DARK_CORE_LIGHTING_MODE_PULSE = 0x01,
+
+  /* Rainbow cycle */
+  DARK_CORE_LIGHTING_MODE_RAINBOW = 0x03,
+
+  /* Static color */
+  DARK_CORE_LIGHTING_MODE_STATIC = 0x07,
+};
